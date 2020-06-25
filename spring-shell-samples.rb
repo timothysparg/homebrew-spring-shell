4b6c5f04a83d87a3f1414ef0379e99682bde8ff1
Tim Sparg <timothy.sparg@gmail.com>
2020-06-25 22:03:20 +0200

    First revison of formulae

---
+++ spring-shell-samples.rb
@@ -0,0 +1,23 @@
+class SpringShellSamples < Formula
+    desc "Spring Shell Samples"
+    homepage "https://github.com/timothysparg/spring-shell"
+    url "https://maven.pkg.github.com/timothysparg/spring-shell/coza/sparg/spring-shell-samples/1.0.1/spring-shell-samples-1.0.1.jar"
+    sha256 "60d61d6899757ae96c6fb2f1f711466983943292214d0667a2ee2d798a715d79"
+    revision 1
+  
+    bottle :unneeded
+  
+    depends_on "openjdk@8"
+  
+    def install
+      libexec.install "spring-shell-samples-1.0.1.jar"
+      (bin/"spring-shell-samples").write <<~EOS
+        #!/bin/bash
+        exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/spring-shell-samples-1.0.1.jar" "$@"
+      EOS
+    end
+  
+    test do
+      system "#{bin}/spring-shell-samples"
+    end
+  end
